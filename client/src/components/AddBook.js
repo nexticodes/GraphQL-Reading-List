1a55d939575fb2fc0b3fafe5731c8a79273c02dc
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-02-06 11:14:00 -0800

    Allow retrieval of data from user input and display data on console to make sure it works. Lesson 30.

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {graphql} from 'react-apollo';
 
 import {getAuthorsQuery} from './../queries/queries';
 
 function AddBook(props) {
+
+    // Declare state slices. You may use state with classes but I want to use react hooks for this.
+    const [name, setName] = useState('');
+    const [genre, setGenre] = useState('');
+    const [authorId, setAuthorId] = useState('');
+
     const displayAuthors = () => {
         let data = props.data;
         if (data.loading){
@@ -17,30 +23,35 @@ function AddBook(props) {
         }
     }
 
+    const submitForm = e => {
+        e.preventDefault();
+        console.log(`${name}, ${genre}, ${authorId}`);
+    }
+
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={submitForm}>
 
-        <div className="field">
-          <label>Book name:</label>
-          <input type="text"/>
-        </div>
-        
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text"/>
-        </div>
+            <div className="field">
+            <label>Book name:</label>
+            <input type="text" onChange={(e) => setName(e.target.value)}/>
+            </div>
+            
+            <div className="field">
+            <label>Genre:</label>
+            <input type="text" onChange={(e) => setGenre(e.target.value)}/>
+            </div>
 
-        <div className="field">
-          <label>Author:</label>
-          <select>
-            <option>Select Author</option>
-            {displayAuthors()}
-          </select>
-        </div>
+            <div className="field">
+            <label>Author:</label>
+            <select onChange={(e) => setAuthorId(e.target.value)}>
+                <option>Select Author</option>
+                {displayAuthors()}
+            </select>
+            </div>
 
-        <button>+</button>
+            <button>+</button>
 
-      </form>
+        </form>
     )
 }
 
