271a5c35bfc9bd276e7118ebbc006657d87fad4e
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-02-06 10:02:49 -0800

    Change CSS, and create a simple component to display books.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Ernie's Reading List</h1>
+      <BookList/>
     </div>
   );
 }
