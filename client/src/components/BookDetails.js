c9f961603380148168bf993a07a63eeba2b2829c
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-02-06 12:18:43 -0800

    Retrieve passed ID from prop. Edit and add options to the query within the graphql export call at the end. Display all the necessary data once retrieved. Lesson 35.

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -3,11 +3,45 @@ import {graphql} from 'react-apollo';
 import {getBookQuery} from '../queries/queries'
 
 function BookDetails(props){
+
+    const displayBookDetails = () => {
+        const { book } = props.data;
+        if (book){
+            return (
+                <div>
+                    <h2>{book.name}</h2>
+                    <p>{book.genre}</p>
+                    <p>{book.author.name}</p>
+                    <p>All the books by the author:</p>
+                    <ul className="other-books">
+                        {book.author.books.map(item => {
+                            return <li key={item.id}>{item.name}</li>
+                        })}
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    No Book Selected.
+                </div>
+            )
+        }
+    }
+
     return (
         <div id="book-details">
-            <p>Output book details here</p>
+            {displayBookDetails()}
         </div>
     )
 }
 
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(getBookQuery, {
+    options: (props) => {
+        return {
+            variables: {
+                id: props.selectedBookId
+            }
+        }
+    }
+})(BookDetails);
